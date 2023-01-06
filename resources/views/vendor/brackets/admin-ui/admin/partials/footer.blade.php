a8e129566ebfe5a6d66468dd87ca448f19e7cd3e
ayuwidyaagata <ayuwagata@gmail.com>
2023-01-06 23:11:58 +0700

    feat: add layout

---
+++ resources/views/vendor/brackets/admin-ui/admin/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="app-footer">
+    <div class="container-fluid">
+        <div class="container-xl">
+            <span class="pull-right">{{ trans('brackets/admin-ui::admin.footer.powered_by') }} Kelompok PKL Kominfo</span>
+            <!-- <span class="pull-right">{{ trans('brackets/admin-ui::admin.footer.powered_by') }} <a href="https://www.getcraftable.com">Craftable</a></span> -->
+        </div>
+    </div>
+</footer>
